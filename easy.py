367aed4c2b1f41e2e6189aa21f63aff81de68180
TakiDev1 <143247500+TakiDev1@users.noreply.github.com>
2024-01-23 21:23:43 +1100

    Update easy.py

--- easy.py
+++ easy.py
@@ -1,6 +1,8 @@
 # easy.py
 
 from utils import bot_ban
+from utils import bot_kick
+from utils import bot_mute
 import discord
 from discord.ext import commands
 
@@ -9,9 +11,21 @@ class EasyCommands(commands.Cog):
         self.bot = bot
 
     @commands.command(name='ban')
-    async def ban_user(self, ctx, user: discord.User):
+    @commands.has_permissions(ban_members = True)
+    async def ban_user(self, ctx, user: discord.User, reason="Banned by a moderator/admin"):
         """Ban a user using a simplified command."""
-        await bot_ban(ctx.guild, user, ctx)
+        await bot_ban(ctx.guild, user, reason)
+    @commands.command(name='kick')
+    @commands.has_permissions(kick_members = True)
+    async def kick_user(self, ctx, user: discord.User):
+        """Kick a user using a simplified command."""
+        await bot_kick(ctx.guild, user, ctx)
+
+    @commands.command(name='mute')
+    @commands.has_permissions(mute_members = True)
+    async def mute_user(self, ctx, user: discord.User):
+        """Mute a user using a simplified command."""
+        await bot_mute(ctx.guild, user, ctx)    
 
 
 
