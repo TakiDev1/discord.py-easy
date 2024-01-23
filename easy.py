6b57637be804ddddd41947f64cf97763caad2fed
TakiDev1 <143247500+TakiDev1@users.noreply.github.com>
2024-01-23 20:23:40 +1100

    Create easy.py

---
+++ easy.py
@@ -0,0 +1,18 @@
+# easy.py
+
+from utils import bot_ban
+import discord
+from discord.ext import commands
+
+class EasyCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='ban')
+    async def ban_user(self, ctx, user: discord.User):
+        """Ban a user using a simplified command."""
+        await bot_ban(ctx.guild, user)
+
+
+
+print("discord.py-easy is working!")
