c9e3145e96c9d4a1154c57f483185688523f510c
TakiDev1 <143247500+TakiDev1@users.noreply.github.com>
2024-01-23 20:25:00 +1100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from easy import bot_ban
+
+# Create a bot instance
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='ban')
+async def ban_user(ctx, user: discord.User):
+    """Ban a user using a simplified command."""
+    await bot_ban(ctx.guild, user)
+
+# Other bot setup code...
+
+# Run the bot
+bot.run('YOUR_BOT_TOKEN')
